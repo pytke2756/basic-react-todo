41410a1aa567b49a47bc115a1ccbf808889bdee0
Levente Ibrányi <ibranyilevente@gmail.com>
2022-09-11 12:46:14 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo"
+
+function App() {
+  return (
+    <div>
+      <Todo text="Learn React"/>
+      <Todo text="More React"/>
+      <Todo text="More more React"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,10 @@
+function Todo(props) {
+    return(
+        <div>
+            <h1>{props.text}</h1>
+            <button>Delete</button>
+        </div>
+    );
+}
+
+export default Todo;
