544663fa32a6c57811b7b23a27add3e406263aab
Levente Ibrányi <ibranyilevente@gmail.com>
2022-09-12 16:57:48 +0200

    event handling

---
+++ src/components/Backdrop.js
@@ -0,0 +1,5 @@
+function Backdrop(props) {
+    return <div onClick={props.onCancel}>asd</div>
+}
+
+export default Backdrop;
---
+++ src/components/Modal.js
@@ -0,0 +1,17 @@
+function Modal(props) {
+    function cancelHandler() {
+        props.onCancel();
+    }
+
+    function confirmHandler() {
+        props.onConfirm();
+    }
+  return (
+    <div>
+      <p>Are you sure?</p>
+      <button onClick={cancelHandler}>Cancel</button>
+      <button onClick={confirmHandler}>Confirm</button>
+    </div>
+  );
+}
+export default Modal;
