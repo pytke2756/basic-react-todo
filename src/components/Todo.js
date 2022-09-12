a71917d1ee1df3b3000e9a94ccaad54c8c16ec2a
Levente Ibrányi <ibranyilevente@gmail.com>
2022-09-12 16:56:50 +0200

    event handling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div>
       <h1>{props.text}</h1>
-      <button>Delete</button>
+      <button onClick={deleteHandler}>Delete</button>
+      {modalIsOpen ? (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      ) : null}
+      {/* modalIsOpen && <Modal /> */}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
